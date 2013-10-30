bebfc3c0932c4eb9998f79c9fa3f3bab43c3066c
Jonah <junyuecao@gmail.com>
2013-10-30 20:12:45 +0800

    多账户支持 发布 0.2版

--- js/app.js
+++ js/app.js
@@ -6,5 +6,6 @@ angular.module('srun3000', ['srun3000.filters', 'srun3000.services', 'srun3000.d
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/main', {templateUrl: 'partials/form.html', controller: 'MainCtrl'});
     $routeProvider.when('/detail', {templateUrl: 'partials/detail.html', controller: 'DetailCtrl'});
+    $routeProvider.when('/add',{templateUrl:'partials/add.html',controller:'AddCtrl'});
     $routeProvider.otherwise({redirectTo: '/main'});
   }]);
--- js/controllers.js
+++ js/controllers.js
@@ -2,8 +2,8 @@
 
 /* Controllers */
 
-angular.module('srun3000.controllers', []).
-  controller('MainCtrl', ['$scope','$routeParams','$http',function($scope,$routeParams,$http) {
+angular.module('srun3000.controllers', [])
+  .controller('MainCtrl', ['$scope','$routeParams','$http',function($scope,$routeParams,$http) {
         chrome.storage.sync.get(function(value) {
             // The $apply is only necessary to execute the function inside Angular scope
             $scope.$apply(function() {
@@ -12,30 +12,42 @@ angular.module('srun3000.controllers', []).
         });
         // If there is saved data in storage, use it. Otherwise, bootstrap with sample todos
         $scope.load = function(value) {
-            if (value && value.account) {
-                $scope.account = value.account;
+            if (value && value.accounts) {
+                $scope.accounts = value.accounts;
+                $scope.current = angular.copy($scope.accounts.ids[$scope.accounts.current]);
                 $scope.uid = value.uid;
             } else {
-                $scope.account = {
-                    username:"",
-                    password:"",
-                    drop:true
+                $scope.accounts = {
+                    current:1,
+                    ids:[{username:"111118",password:"880824"},{username:"hiyuki",password:"3113979"}]
                 };
             }
         };
         $scope.saveAccount = function() {
-            chrome.storage.sync.set({'account': $scope.account});
+            var has = false;//是否已经在列表中
+            $scope.accounts.ids.forEach(function(e,i){
+                if($scope.current.username == e.username){
+                    has = true;
+                    $scope.accounts.current = i;
+                }
+            });
+            if(!has){
+                $scope.accounts.ids.push(angular.copy($scope.current));
+                $scope.accounts.current = $scope.accounts.ids.length -1;
+            }
+            chrome.storage.sync.set({'accounts': $scope.accounts});
         };
 
         $scope.message = "上网不涉密,涉密不上网!";
+        $scope.openDiv = false;
         $scope.save = true;
 
 
         $scope.login = function(){
-
-            var pass1=hex_md5($scope.account.password);
+            var curUser = $scope.accounts.ids[$scope.accounts.current];
+            var pass1=hex_md5(curUser.password);
             var pass2=pass1.substr(8,16);
-            var data = "username="+$scope.account.username+"&password="+pass2+"&drop="+($scope.account.drop?1:0)+"&type=1&n=100";
+            var data = "username="+curUser.username+"&password="+pass2+"&type=1&n=100";
 
             $http.post("http://10.0.0.55/cgi-bin/do_login",data
             ,{
@@ -49,15 +61,20 @@ angular.module('srun3000.controllers', []).
                         chrome.storage.sync.set({'uid':data});
                     }
                     if($scope.save){
-                        $scope.saveAccount();
+                        // $scope.saveAccount();
                     }
                 }).error(function(){
                     $scope.message = "timeout";
                 });
         }
 
+        $scope.open = function(){
+            $scope.openDiv = !$scope.openDiv;
+        }
+        
         $scope.logout = function(){
-            var data="username="+$scope.account.username+"&password="+$scope.account.password+"&drop="+($scope.account.drop?1:0)+"&type=1&n=1";
+            var curUser = $scope.accounts.ids[$scope.accounts.current];
+            var data="username="+curUser.username+"&password="+curUser.password+"&type=1&n=1";
 
             $http.post("http://10.0.0.55/cgi-bin/force_logout",data
                 ,{
@@ -71,6 +88,16 @@ angular.module('srun3000.controllers', []).
                     $scope.message = "timeout";
                 });
         }
+        $scope.delete = function(){
+            var ids = $scope.accounts.ids.splice($scope.accounts.current,1);
+            if($scope.accounts.current == $scope.accounts.ids.length){
+                $scope.accounts.current = 0;
+            }
+            chrome.storage.sync.set({'accounts': $scope.accounts});
+            $scope.openDiv = false;
+        }
+
+
 
   }])
   .controller('DetailCtrl', ['$scope','$routeParams','$http',function($scope,$routeParams,$http) {
@@ -87,6 +114,47 @@ angular.module('srun3000.controllers', []).
             }
             var url = "http://10.0.0.55/user_info.php?uid="+ $scope.uid;
             $scope.url = url;
+        };
+
+
+  }])
+  .controller('AddCtrl', ['$scope','$routeParams','$location',function($scope,$routeParams,$location) {
+        $scope.newAccount={username:"",password:""};
+        chrome.storage.sync.get('accounts',function(value) {
+            // The $apply is only necessary to execute the function inside Angular scope
+            $scope.$apply(function() {
+                $scope.load(value);
+            });
+        });
+        // If there is saved data in storage, use it. Otherwise, bootstrap with sample todos
+        $scope.load = function(value) {
+            if (value && value.accounts) {
+                $scope.accounts = value.accounts;
+            }else{
+                $scope.accounts = {
+                    current:1,
+                    ids:[{username:"111118",password:"880824"},{username:"hiyuki",password:"3113979"}]
+                };
+            } 
+        };
+        $scope.add = function() {
+            if($scope.newAccount.username == "" || $scope.newAccount.password == ""){
+                return;
+            }
+            var has = false;//是否已经在列表中
+            $scope.accounts.ids.forEach(function(e,i){
+                if($scope.newAccount.username == e.username){
+                    has = true;
+                    $scope.accounts.current = i;
+                }
+            });
+            if(!has){
+                console.log($scope.newAccount.username);
+                $scope.accounts.ids.push(angular.copy($scope.newAccount));
+                $scope.accounts.current = $scope.accounts.ids.length -1;
+            }
+            chrome.storage.sync.set({'accounts': $scope.accounts});
+            $location.path("#/main");
         };
 
 
